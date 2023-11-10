29ce408c91d0f198761072fa483e03963ab14eab
Sajeda Akter <sajeda16akter@gmail.com>
2023-11-10 17:00:36 +0600

    'donation-site'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { router } from "./components/Route/Router"
+import {
+  
+  RouterProvider,
+} from "react-router-dom";
+function App() {
+
+  return (
+   <RouterProvider router={router}>
+
+   </RouterProvider>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+           <p className="text-3xl">This is home</p> 
+        </div>
+    );
+};
+
+export default Home;
